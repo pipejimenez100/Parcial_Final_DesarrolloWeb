f276c9b71cb89793a2d4084ba882611507bcf68d
pipejimenez180 <pipejimenez100@gmail.com>
2022-05-26 19:31:51 -0500

    +1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="bg-slate-300 text-black h-screen flex text-white">
+    <div className="bg-emerald-500 text-black h-screen flex text-white">
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
